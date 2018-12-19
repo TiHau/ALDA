5c8748c80b7b6637e6a5b6ee139ba2a458458ffb
TiHau <tim.hauenstein97@gmx.de>
2018-12-19 12:32:16 +0100

    A3 init

---
+++ A3/src/ShortestPath.java
@@ -0,0 +1,84 @@
+// O. Bittel;
+// 18.10.2011
+
+package shortestPath;
+
+import graph.*;
+import sim.SYSimulation;
+// ...
+
+/**
+ * Kürzeste Wege in Graphen mit A*- und Dijkstra-Verfahren.
+ * @author Oliver Bittel
+ * @since 27.01.2015
+ * @param <V> Knotentyp.
+ */
+public class ShortestPath<V> {
+	
+	SYSimulation sim = null;
+	
+	Map<V,Double> dist; // Distanz für jeden Knoten
+	Map<V,V> pred; // Vorgänger für jeden Knoten
+	// ...
+
+	/**
+	 * Berechnet im Graph g kürzeste Wege nach dem A*-Verfahren.
+	 * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
+	 * Wird h = null gewählt, dann ist das Verfahren identisch mit dem Dijkstra-Verfahren.
+	 * @param g Gerichteter Graph
+	 * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
+	 * dem Dijkstra-Verfahren gesucht.
+	 */
+	public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
+		// ...
+	}
+
+	/**
+	 * Diese Methode sollte nur verwendet werden, 
+	 * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
+	 * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
+	 * <p>
+	 * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
+	 * <p><blockquote><pre>
+	 *    if (sim != null)
+	 *       sim.visitStation((Integer) v, Color.blue);
+	 * </pre></blockquote>
+	 * @param sim SYSimulation-Objekt.
+	 */
+	public void setSimulator(SYSimulation sim) {
+		this.sim = sim;
+	}
+
+	/**
+	 * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
+	 * <p>
+	 * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
+	 * der als nächstes aus der Kandidatenliste besucht wird, animiert.
+	 * @param s Startknoten
+	 * @param g Zielknoten
+	 */
+	public void searchShortestPath(V s, V g) {
+		// ...
+	}
+
+	/**
+	 * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
+	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+	 * @return kürzester Weg als Liste von Knoten.
+	 */
+	public List<V> getShortestPath() {
+		// ...
+	}
+
+	/**
+	 * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
+	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+	 * @return Länge eines kürzesten Weges.
+	 */
+	public double getDistance() {
+		// ...
+	}
+
+}
